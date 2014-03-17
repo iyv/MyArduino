f6ede5824a4da5b412893e0bf9a65a97884ce619
Ivanov Yuriy <arsenal-izh@mail.ru>
2014-03-17 16:42:08 +0400

    тест всех датчиков

--- InfoHome/InfoHome_arduino/InfoHome_arduino.ino
+++ InfoHome/InfoHome_arduino/InfoHome_arduino.ino
@@ -1,7 +1,17 @@
-// I assume you know how to connect the DS1302 and LCD.
-// DS1302:  CE pin    -> Arduino Digital 2
-//          I/O pin   -> Arduino Digital 3
-//          SCLK pin  -> Arduino Digital 4
+/* I assume you know how to connect the DS1302 and LCD.
+
+ DS1302:  CE pin    -> Arduino Digital 11
+          I/O pin   -> Arduino Digital 10
+          SCLK pin  -> Arduino Digital 9
+
+  LCD RS  pin to digital pin 6
+  LCD En  pin to digital pin 8
+  LCD D4  pin to digital pin 2
+  LCD D5  pin to digital pin 3
+  LCD D6  pin to digital pin 4
+  LCD D7  pin to digital pin 5
+  LCD R/W pin to digital pin 7
+*/
 
 #include <OneWire.h>
 #include <DS1302.h>
@@ -9,47 +19,70 @@
 #include <dht.h>
 
 OneWire  ds(12);  // линия 1-Wire будет на pin 12
-// initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(12, 11, 6, 7, 8, 9);
+LiquidCrystal lcd(6, 8, 2, 3, 4, 5); // initialize the library with the numbers of the interface pins
+
 /* Set the appropriate digital I/O pin connections */
-uint8_t CE_PIN   = 3;
-uint8_t IO_PIN   = 4;
-uint8_t SCLK_PIN = 5;
+DS1302 rtc(11, 10, 9); // Create a DS1302 object
 
 /* Create buffers */
-char buf[50];
-char day[10];
-int i = 0;
+//char buf[50];
+//char day[10];
+int i = 1;
 
-/* Create a DS1302 object */
-DS1302 rtc(CE_PIN, IO_PIN, SCLK_PIN);
+
 // создаём объект-сенсор
 DHT sensor = DHT();
 
-byte m;
-byte data[12];
+byte NMenu = 0; //переменная для хранения пункта меню
+int RW = 7;//вывод разрещения записи в дисплей
+byte m = 0;//простой счетчик
+byte data[12];//массив данных с датчика ds18b20
 byte type_s;
-byte addr[8];
-byte addr1[8]= {0x28, 0x3C, 0x5B, 0x47, 0x02, 0x00, 0x00, 0x42}; 
-byte addr2[8]= {0x28, 0x75, 0x10, 0x60, 0x04, 0x00, 0x00, 0x1F};
-float celsius;
+byte addr[8];//массив с адресом датчика ds18b20
+byte addr1[8]= {0x28, 0x3C, 0x5B, 0x47, 0x02, 0x00, 0x00, 0x42}; //первый датчик ds18b20
+byte addr2[8]= {0x28, 0x75, 0x10, 0x60, 0x04, 0x00, 0x00, 0x1F}; //второй датчик ds18b20
+float celsius; //переменая для перевода в градусы цельсия
 
+//--------------------------------------------------------------
 void setup(void) 
 {
   // set up the LCD's number of columns and rows: 
+  pinMode(RW, OUTPUT);
+  digitalWrite(RW, LOW);
   lcd.begin(16, 2);
   sensor.attach(A0); 
+
+  lcd.clear();
+  for (i=0; i<16; i++) {
+      lcd.setCursor(i, 0);
+      lcd.print(char(255));
+      lcd.setCursor(16-i, 1);
+      lcd.print(char(255));
+      delay(100);
+    };      
   /*
-  rtc.write_protect(false);
+  rtc.writeProtect(false);
   rtc.halt(false);
-  //   Tuesday, May 19, 2009 at 21:16:37.            
-  Time t(2012, 3, 4, 22, 37, 0, 2);
-  // Set the time and date on the chip *
-  rtc.time(t);*/
-  
+  rtc.setDOW(7);        // Set Day-of-Week to FRIDAY
+  rtc.setTime(16, 8, 0);     // Set the time to 12:00:00 (24hr format)
+  rtc.setDate(3, 9, 2014);   // Set the date to August 6th, 2010
+  rtc.writeProtect(true);
+  rtc.halt(true);
+  */
+ /* 
+  lcd.clear();
+  for (i=1; i<256; i++) {
+    lcd.setCursor(1,0);
+    lcd.print (i);
+    lcd.setCursor(5,1);
+    lcd.print(char(i));
+    delay(200);
+    };
+  */
   Serial.begin(57600);
 }
 
+//--------------------------------------------------------------
 void loop(void) 
 {
   lcd.clear();
@@ -72,19 +105,34 @@ void loop(void)
      break;
     }
   }
-    printDHT11();
+  print_DateTime();  
   delay(1000);
-  i = i + 1;
-  if (i == 5) {
-    print_DateTime();
-    } 
+  m = m + 1;
+  if (m == 3) {
+    printDHT11();
+    }
+  if (m == 6) {
+    printDS18B20();
+    m=1;
+    delay(1000);
+    }  
 }  
 
-
+//--------------------------------------------------------------
 void printDS18B20()
 {  
+  lcd.clear();
+  lcd.setCursor(0,0);
+  if ( !ds.search(addr)) {
+    //lcd.println("No more addresses.");
+    //Serial.println();
+    ds.reset_search();
+    delay(250);
+    return;
+  }
+  
   if ( OneWire::crc8( addr, 7) != addr[7]) {
-        Serial.print("CRC is not valid!\n");
+        lcd.print("CRC is not valid!\n");
     return;
     }
   ds.reset();
@@ -115,9 +163,13 @@ void printDS18B20()
     //// default is 12 bit resolution, 750 ms conversion time
   }
   celsius = (float)raw / 16.0;
-  Serial.println(celsius);
+  lcd.setCursor(0, 0);
+  lcd.print("Temp= ");
+  lcd.print(celsius);
+  lcd.print("C");
 }
 
+//--------------------------------------------------------------
 void printDHT11()
 {
   // метод update заставляет сенсор выдать текущие измерения
@@ -153,20 +205,64 @@ void printDHT11()
   i=0;
 }
 
+//--------------------------------------------------------------
 void print_DateTime()
 {
   // Display time centered on the upper line
+  //lcd.setCursor(0, 0);
+  //lcd.print("/");
   lcd.setCursor(4, 0);
   lcd.print(rtc.getTimeStr());
   
   // Display abbreviated Day-of-Week in the lower left corner
   lcd.setCursor(0, 1);
-  lcd.print(rtc.getDOWStr(FORMAT_SHORT));
+  lcd.print(rtc.getDOWStr(1));
   
   // Display date in the lower right corner
-  lcd.setCursor(6, 1);
-  lcd.print(rtc.getDateStr());
+  lcd.setCursor(7, 1);
+  lcd.print(rtc.getDateStr(FORMAT_SHORT));
 
   // Wait one second before repeating :)
-  //delay (1000);
+  delay (1000);
+}
+
+void print_Menu()
+{
+  switch (NMenu)
+  {
+    case 1:
+      lcd.setCursor(0, 0);
+      lcd.print ("<   Set TIME   >");
+      break;
+    case 2:
+      lcd.setCursor(0, 0);
+      lcd.print ("     Set HH");
+      lcd.setCursor(0, 1);
+      lcd.print ("<");
+      lcd.setCursor(6, 1);
+      lcd.print ("00");
+      lcd.setCursor(15, 1);
+      lcd.print (">");
+      break;
+    case 3:
+      lcd.setCursor(0, 0);
+      lcd.print ("     Set MM");
+      lcd.setCursor(0, 1);
+      lcd.print ("<");
+      lcd.setCursor(6, 1);
+      lcd.print ("00");
+      lcd.setCursor(15, 1);
+      lcd.print (">");
+      break;
+    case 4:
+      lcd.setCursor(0, 0);
+      lcd.print ("     Set SS");
+      lcd.setCursor(0, 1);
+      lcd.print ("<");
+      lcd.setCursor(6, 1);
+      lcd.print ("00");
+      lcd.setCursor(15, 1);
+      lcd.print (">");
+      break;
+  }
 }
